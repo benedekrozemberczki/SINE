50dbb8d39d0cf11ad29668fbea7c6c8a83002151
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-08 15:59:06 +0000

    Update walkers.py

--- src/walkers.py
+++ src/walkers.py
@@ -7,6 +7,11 @@ class RandomWalker:
     Class to generate vertex sequences.
     """
     def __init__(self, graph, repetitions, length):
+        """
+        :param graph: Networkx graph.
+        :param repetitions: Number of truncated random walks per source node.
+        :param length: Length of truncated random walks.
+        """
         print("Model initialization started.")
         self.graph = graph
         self.repetitions = repetitions 
@@ -15,6 +20,8 @@ class RandomWalker:
     def small_walk(self, start_node):
         """
         Generate a node sequence from a start node.
+        :param start_node: The source node.
+        :return walk: Truncated random walk.
         """
         walk = [start_node]
         while len(walk) != self.length:
