c844c686fc10feb03bd3f56f19a12fe0f9022000
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-08-04 19:42:09 +0100

    Update walkers.py

--- src/walkers.py
+++ src/walkers.py
@@ -17,21 +17,18 @@ class RandomWalker:
         self.graph = graph
         self.repetitions = repetitions 
         self.length = length
-
+        
     def small_walk(self, start_node):
         """
-        Generate a node sequence from a start node.
-        :param start_node: The source node.
-        :return walk: Truncated random walk.
+        Doing a truncated random walk.
+        :param start_node: Start node for random walk.
+        :return walk: Truncated random walk with fixed maximal length.
         """
         walk = [start_node]
-        while len(walk) != self.length:
-            end_point = walk[-1]
-            neighbors = nx.neighbors(self.graph, end_point)
-            if len(neighbors) > 0:
-                walk = walk + random.sample(neighbors, 1)
-            else:
+        while len(walk) < self.args.walk_length:
+            if len(nx.neighbors(self.graph,walk[-1])) == 0:
                 break
+            walk = walk + [random.sample(nx.neighbors(self.graph,walk[-1]),1)[0]]
         return walk
        
     def do_walks(self):
